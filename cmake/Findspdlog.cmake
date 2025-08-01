7a649dbbd81ebbf7c436b62dd540c20731ec9dca
lyn <waaaaard@qq.com>
2025-08-01 17:32:09 +0800

    refine pkg

--- cmake/Findspdlog.cmake
+++ cmake/Findspdlog.cmake
@@ -8,6 +8,7 @@ FetchContent_Declare(
 )
 message(STATUS "fetching spdlog")
 FetchContent_MakeAvailable(spdlog)
+target_compile_definitions(spdlog PRIVATE _CRT_DECLARE_NONSTDC_NAMES)
 
 if(TARGET spdlog::spdlog)
     message(STATUS "found target: spdlog::spdlog")
